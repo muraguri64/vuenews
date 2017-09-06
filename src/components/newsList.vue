7616b17cd75f52eb4979d26e38e2f465c81114d5
muraguri64 <muraguri64@gmail.com>
2017-09-06 12:53:56 +0300

    Add newsList and sourceSelection components

---
+++ src/components/newsList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="newsList">newsList</div>
+</template>
+
+<script>
+
+export default {
+  name: 'newsList'
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/sourceSelection.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="sourceSelection">sourceSelection</div>
+</template>
+
+<script>
+
+export default {
+  name: 'sourceSelection'
+}
+
+</script>
+
+<style scoped>
+</style>
